023a2a8b5c01b5e3c97935dd9e43446ded92865f
loodaobao <warren.y.cheng@gmail.com>
2023-09-17 16:46:33 +0900

    add tests for all admin funcs

--- tests/mydandefi.test.ts
+++ tests/mydandefi.test.ts
@@ -1,8 +1,8 @@
 import { assert, describe, test, clearStore, beforeAll, afterAll } from "matchstick-as/assembly/index";
 import { Address, BigInt } from "@graphprotocol/graph-ts";
-import { MembershipInserted, MembershipUpdated } from "../generated/MyDanDefi/MyDanDefi";
-import { handleMembershipInserted, handleMembershipUpdated } from "../src/mydandefi";
-import { createMembershipInsertedEvent, createMembershipUpdatedEvent } from "./mydandefi-utils";
+import { DurationBonusRateUpdated, MembershipInserted, MembershipUpdated, ReferralBonusRateUpdated } from "../generated/MyDanDefi/MyDanDefi";
+import { handleMembershipInserted, handleMembershipUpdated, handleDurationBonusRateUpdated, handleReferralBonusRateUpdated } from "../src/mydandefi";
+import { createDurationBonusRateUpdatedEvent, createMembershipInsertedEvent, createMembershipUpdatedEvent, createReferralBonusRateUpdatedEvent } from "./mydandefi-utils";
 
 describe("test mydandefi event handlers", () => {
   beforeAll(() => {});
@@ -28,5 +28,17 @@ describe("test mydandefi event handlers", () => {
     handleMembershipUpdated(event);
     assert.entityCount("MembershipTier", 2);
     assert.fieldEquals("MembershipTier", "0x1", "name", "test3");
+  });
+  test("test handleDurationBonusRateUpdated", () => {
+    let event: DurationBonusRateUpdated = createDurationBonusRateUpdatedEvent(BigInt.fromI32(1), BigInt.fromI32(1));
+    handleDurationBonusRateUpdated(event);
+    assert.entityCount("Duration", 1);
+    assert.fieldEquals("Duration", "0x1", "bonusDepositInterestRate", "1");
+  });
+  test("test handleReferralBonusRateUpdated", () => {
+    let event: ReferralBonusRateUpdated = createReferralBonusRateUpdatedEvent(BigInt.fromI32(1), BigInt.fromI32(1));
+    handleReferralBonusRateUpdated(event);
+    assert.entityCount("ReferralLevel", 1);
+    assert.fieldEquals("Duration", "0x1", "bonusDepositInterestRate", "1");
   });
 });
