f804f8569a83d2f264156d3d24797c9f6ced7622
loodaobao <warren.y.cheng@gmail.com>
2023-09-17 18:21:10 +0900

    beforeAll to beforeEach to separate each test without dependency

--- tests/mydandefi.test.ts
+++ tests/mydandefi.test.ts
@@ -1,4 +1,4 @@
-import { assert, describe, test, clearStore, beforeAll, afterAll } from "matchstick-as/assembly/index";
+import { assert, describe, test, clearStore, beforeAll, afterAll, afterEach,beforeEach } from "matchstick-as/assembly/index";
 import { Address, BigInt } from "@graphprotocol/graph-ts";
 import { DurationBonusRateUpdated, MembershipInserted, MembershipUpdated, ReferralBonusRateUpdated } from "../generated/MyDanDefi/MyDanDefi";
 import { handleMembershipInserted, handleMembershipUpdated, handleDurationBonusRateUpdated, handleReferralBonusRateUpdated, handlePassMinted, handleReferralCodeCreated } from "../src/mydandefi";
@@ -16,13 +16,13 @@ import { createTransferEvent } from "./mydanpass-utils";
 let zeroAddress: Address;
 let toAddress: Address;
 describe("test mydandefi event handlers", () => {
-  beforeAll(() => {
+  beforeEach(() => {
     zeroAddress = Address.fromString("0x0000000000000000000000000000000000000000");
     toAddress = Address.fromString("0x0000000000000000000000000000000000000001");
     handleTransfer(createTransferEvent(zeroAddress, toAddress, BigInt.fromI32(0)));
   });
 
-  afterAll(() => {
+  afterEach(() => {
     clearStore();
   });
 
@@ -34,15 +34,19 @@ describe("test mydandefi event handlers", () => {
     assert.fieldEquals("Profile", "0x0", "membershipTier", "0x0");
   });
   test("test second handleMembershipInserted", () => {
-    let event: MembershipInserted = createMembershipInsertedEvent(BigInt.fromI32(1), "test2", BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0));
-    handleMembershipInserted(event);
+    let fristEvent: MembershipInserted = createMembershipInsertedEvent(BigInt.fromI32(0), "test", BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0));
+    handleMembershipInserted(fristEvent);
+    let secondEvent: MembershipInserted = createMembershipInsertedEvent(BigInt.fromI32(1), "test2", BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0));
+    handleMembershipInserted(secondEvent);
     assert.entityCount("MembershipTier", 2);
     assert.fieldEquals("MembershipTier", "0x1", "name", "test2");
   });
   test("test handleMembershipUpdated", () => {
+    let membershipInsertedEvent: MembershipInserted = createMembershipInsertedEvent(BigInt.fromI32(1), "test2", BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0));
+    handleMembershipInserted(membershipInsertedEvent)
     let event: MembershipUpdated = createMembershipUpdatedEvent(BigInt.fromI32(1), "test3", BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0));
     handleMembershipUpdated(event);
-    assert.entityCount("MembershipTier", 2);
+    assert.entityCount("MembershipTier", 1);
     assert.fieldEquals("MembershipTier", "0x1", "name", "test3");
   });
   test("test handleDurationBonusRateUpdated", () => {
@@ -55,9 +59,13 @@ describe("test mydandefi event handlers", () => {
     let event: ReferralBonusRateUpdated = createReferralBonusRateUpdatedEvent(BigInt.fromI32(1), BigInt.fromI32(1));
     handleReferralBonusRateUpdated(event);
     assert.entityCount("ReferralLevel", 1);
-    assert.fieldEquals("Duration", "0x1", "bonusDepositInterestRate", "1");
+    assert.fieldEquals("ReferralLevel", "0x1", "referralBonusRate", "1");
   });
   test("test handlePassMinted", () => {
+    // create non tier
+    let noneTier: MembershipInserted = createMembershipInsertedEvent(BigInt.fromI32(0), "None", BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0), BigInt.fromI32(0));
+    handleMembershipInserted(noneTier);
+    // create genesis user
     assert.entityCount("User", 1);
     let tokenId = BigInt.fromI32(1);
     let referrerTokenId = BigInt.fromI32(0);
@@ -69,6 +77,12 @@ describe("test mydandefi event handlers", () => {
     assert.fieldEquals("Profile", "0x1", "tokenId", "1");
   });
   test("test handleReferralCodeCreated", () => {
+    // mint one user pass
+    let tokenId = BigInt.fromI32(1);
+    let referrerTokenId = BigInt.fromI32(0);
+    let minter = (toAddress = Address.fromString("0x0000000000000000000000000000000000000003"));
+    let passMintedEvent = createPassMintedEvent(minter, tokenId, referrerTokenId);
+    handlePassMinted(passMintedEvent);
     let event = createReferralCodeCreatedEvent("hello", BigInt.fromI32(1));
     handleReferralCodeCreated(event);
     assert.fieldEquals("Profile", "0x1", "referralCode", "hello");
